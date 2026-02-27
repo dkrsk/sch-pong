fed5817b4fdfa7be5d97e44b6683a28750d5a878
Angle Muad <wileps2310@yandex.ru>
2026-02-27 20:45:16 +0300

    feature/draw Moving constants to directives

--- src/pong.c
+++ src/pong.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
+#define SCR_WD 80
+#define SCR_HG 25
+#define PDL_SIZE 3
 
 // --- ЗОНА 1: Отрисовка ---
 void Draw(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2) {
-    const int screen_width = 80;
-    const int screen_height = 25;
-    const int paddle_size = 3;
     const int score_y_pos = 7;
 
     printf("\e[H");  // Clear space
 
-    for (int y = 0; y < screen_height; y++) {
-        for (int x = 0; x < screen_width; x++) {
+    for (int y = 0; y < SCR_HG; y++) {
+        for (int x = 0; x < SCR_WD; x++) {
             if (x == ball_x && y == ball_y) {  // Ball
                 printf("*");
-            } else if (x == 0 && y >= player1_pos && y < player1_pos + paddle_size) {
+            } else if (x == 0 && y >= player1_pos && y < player1_pos + PDL_SIZE) {
                 printf("|");  // Left paddle
-            } else if (x == screen_width - 1 && y >= player2_pos && y < player2_pos + paddle_size) {
+            } else if (x == SCR_WD - 1 && y >= player2_pos && y < player2_pos + PDL_SIZE) {
                 printf("|");  // Right paddle
-            } else if (x == screen_width / 2) {
+            } else if (x == SCR_WD / 2) {
                 printf("|");  // Separator
-            } else if (x == screen_width / 2 - 3 && y == score_y_pos) {
+            } else if (x == SCR_WD / 2 - 3 && y == score_y_pos) {
                 int offset = printf("%d", score1);  // Score plr1
                 x += offset - 1;
-            } else if (x == screen_width / 2 + 3 && y == score_y_pos) {
+            } else if (x == SCR_WD / 2 + 3 && y == score_y_pos) {
                 int offset = printf("%d", score2);  // Score plr2
                 x += offset - 1;
             } else
