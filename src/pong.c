b704fad5c5b5b299208dc25993677c709af500bd
Angle Muad <wileps2310@yandex.ru>
2026-02-27 12:40:59 +0300

    Create the basic architecture

--- src/pong.c
+++ src/pong.c
@@ -1,5 +1,14 @@
-//I WANT TO PLAY WITH YOU
-//        YOUR FRIEND, AI
+#include <stdio.h>
 
+// --- ЗОНА 1: Отрисовка ---
+void Draw(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2);
 
+// --- ЗОНА 2: Управление и Физика ---
+// Смена bx, by и проверка столкновений. Возможно необходимо изменить набор параметров
+void Update(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2);
 
+int main() {
+    // Инициализация и бесконечный цикл: draw -> update
+    // Проверка score и выбор победителя
+    return 0;
+}
