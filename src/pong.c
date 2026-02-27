66382ea800458cbf7635d105bba8c3c5990f96d4
Angle Muad <wileps2310@yandex.ru>
2026-02-27 18:01:02 +0300

    Change architect

--- src/pong.c
+++ src/pong.c
@@ -3,9 +3,10 @@
 // --- ЗОНА 1: Отрисовка ---
 void Draw(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2);
 
-// --- ЗОНА 2: Управление и Физика ---
-// Смена bx, by и проверка столкновений. Возможно необходимо изменить набор параметров
-void Update(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2);
+// --- ЗОНА 2: Обработка ввода ---
+int Input();
+int DetectWhitchPlayer(int input);
+int GetDirectionByInput(int input);
 
 int main() {
     // Инициализация и бесконечный цикл: draw -> update
