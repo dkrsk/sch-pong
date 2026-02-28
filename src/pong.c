377dc6a385d3b40e40fc8963b982174b4b37c7b3
Angle Muad <wileps2310@yandex.ru>
2026-02-28 11:45:47 +0300

    Add directions and score funcs

--- src/pong.c
+++ src/pong.c
@@ -8,6 +8,14 @@ int Input();
 int DetectWhitchPlayer(int input);
 int GetDirectionByInput(int input);
 
+// --- ЗОНА 3: Обработка ориентации движения ---
+int DirectionX(int x, int ball_x, int ball_y, int player1_pos, int player2_pos, int paddle_size, int screen_width);
+int DirectionY(int y, int ball_y, int screen_height);
+
+// -- ЗОНА 4: Проверка гола и победы --
+int CheckWin(int score1, int score2);
+int CheckGoal(int ball_x, int ball_y);
+
 int main() {
     // Инициализация и бесконечный цикл: draw -> update
     // Проверка score и выбор победителя
