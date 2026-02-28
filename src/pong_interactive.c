3a63b44676fd4ab4bec01b6ac056df192b315292
Angle Muad <wileps2310@yandex.ru>
2026-02-28 17:16:14 +0300

    INTERACTIVVVEEE

--- src/pong_interactive.c
+++ src/pong_interactive.c
@@ -1,43 +1,37 @@
-#include <stdio.h>
 #include <ncurses.h>
 #define SCR_WD 80
 #define SCR_HG 25
 #define PDL_SIZE 3
 #define WIN_SCR 21
+#define BL_SPEED 3
 
-// --- ЗОНА 1: Отрисовка ---
 // --- ЗОНА 1: Отрисовка (Интерактивная) ---
 void Draw(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2) {
-    const int score_y_pos = 1; // Поднимем счет повыше для удобства
+    const int score_y_pos = 1;
 
-    // Быстрая очистка внутреннего буфера ncurses
-    erase(); 
+    erase();
 
-    // 1. Отрисовка центральной линии (разделителя)
     for (int y = 0; y < SCR_HG; y++) {
         mvaddch(y, SCR_WD / 2, '|');
     }
 
-    // 2. Отрисовка ракеток (только там, где они есть)
     for (int i = 0; i < PDL_SIZE; i++) {
-        mvaddch(player1_pos + i, 0, '#');           // Левая (используем # для видимости)
-        mvaddch(player2_pos + i, SCR_WD - 1, '#');  // Правая
+        mvaddch(player1_pos + i, 0, '|');
+        mvaddch(player2_pos + i, SCR_WD - 1, '|');
     }
 
-    // 3. Отрисовка счета
     mvprintw(score_y_pos, SCR_WD / 2 - 5, "%d", score1);
     mvprintw(score_y_pos, SCR_WD / 2 + 4, "%d", score2);
 
-    // 4. Отрисовка мяча
-    mvaddch(ball_y, ball_x, 'O');
+    mvaddch(ball_y, ball_x, '*');
 
-    // Перенос всех изменений из буфера на физический экран терминала
-    refresh(); 
+    refresh();
 }
 
 void DrawWin(int winner) {
-    clear(); // Полная очистка перед финальным экраном
-    
+    nodelay(stdscr, FALSE);
+    clear();
+
     // Вместо \e[H используем move(0,0) или просто mvprintw
     int start_y = SCR_HG / 2 - 4;
     int start_x = SCR_WD / 2 - 20;
@@ -57,27 +51,25 @@ void DrawWin(int winner) {
         mvprintw(start_y + 4, start_x, " | |     | |    \\  /\\  /   _| |_| |\\  |_|");
         mvprintw(start_y + 5, start_x, " |_|     |_|     \\/  \\/   |_____|_| \\_(_)");
     }
-    
+
     mvprintw(SCR_HG - 2, SCR_WD / 2 - 12, "Press any key to exit...");
     refresh();
+    getch();
+    endwin();
 }
 // --- ЗОНА 2: Проверка ввода ---
 
 int Input() {
-    int input_code = 0;
+    int last_input = 0;
+    int kbs;
 
-    char kbs = getch();
-
-        if (kbs == 'A' || kbs == 'a') {
-            input_code = -11;
-        } else if (kbs == 'Z' || kbs == 'z') {
-            input_code = 11;
-        } else if (kbs == 'K' || kbs == 'k') {
-            input_code = -12;
-        } else if (kbs == 'M' || kbs == 'm') {
-            input_code = 12;
-        }
-    return input_code;
+    while ((kbs = getch()) != ERR) {
+        if (kbs == 'A' || kbs == 'a') last_input = -11;
+        if (kbs == 'Z' || kbs == 'z') last_input = 11;
+        if (kbs == 'K' || kbs == 'k') last_input = -12;
+        if (kbs == 'M' || kbs == 'm') last_input = 12;
+    }
+    return last_input;
 }
 
 int DetectWhichPlayer(int input) {
@@ -96,8 +88,6 @@ int GetDirectionByInput(int input) {
     }
     return dir;
 }
-
-//
 
 // --- ЗОНА 3: Обработка ориентации движения ---
 int GetBallDirectionX(int x, int ball_x, int ball_y, int player1_pos, int player2_pos) {
@@ -148,8 +138,9 @@ void interective() {
     initscr();
     noecho();
     curs_set(0);
-    nodelay(stdscr,TRUE);
+    nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
+    timeout(40);
 }
 int main() {
     interective();
@@ -158,8 +149,7 @@ int main() {
     int ball_x_direction = -1, ball_y_direction = 1;
     int player1_pos = SCR_HG / 2, player2_pos = SCR_HG / 2;
     int score1 = 0, score2 = 0, winner;
-    timeout(100);
-
+    int frame_counter = 0;
 
     while ((winner = CheckWin(score1, score2)) == -1) {
         Draw(ball_x, ball_y, player1_pos, player2_pos, score1, score2);
@@ -173,22 +163,24 @@ int main() {
             player2_pos += HandlePaddleDirection(player_direction, player2_pos);
         }
 
-        ball_x_direction = GetBallDirectionX(ball_x_direction, ball_x, ball_y, player1_pos, player2_pos);
-        ball_y_direction = GetBallDirectionY(ball_y_direction, ball_y);
-        ball_x += ball_x_direction;
-        ball_y += ball_y_direction;
+        if (frame_counter % BL_SPEED == 0) {
+            ball_x_direction = GetBallDirectionX(ball_x_direction, ball_x, ball_y, player1_pos, player2_pos);
+            ball_y_direction = GetBallDirectionY(ball_y_direction, ball_y);
+            ball_x += ball_x_direction;
+            ball_y += ball_y_direction;
 
-        int goal_result = CheckGoal(ball_x, ball_y);
-        if (goal_result != 0) {
-            int tmp1 = goal_result % 10;
-            int tmp2 = goal_result / 10;
-            score1 += tmp1;
-            score2 += tmp2;
-            ball_x = SCR_WD / 2 + 2 * tmp1 - 2 * tmp2;
-            ball_y = SCR_HG / 2;
+            int goal_result = CheckGoal(ball_x, ball_y);
+            if (goal_result != 0) {
+                int tmp1 = goal_result % 10;
+                int tmp2 = goal_result / 10;
+                score1 += tmp1;
+                score2 += tmp2;
+                ball_x = SCR_WD / 2 + 2 * tmp1 - 2 * tmp2;
+                ball_y = SCR_HG / 2;
+            }
         }
+        frame_counter++;
     }
     DrawWin(winner);
-    endwin();
     return 0;
 }
