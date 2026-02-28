4c23c5b411dc78cc4ea6d5e797d4d0142a4673bf
Loreza Orbert <shou_2020@mail.ru>
2026-02-28 16:38:13 +0300

    interactive testbonus

--- src/pong_interactive.c
+++ src/pong_interactive.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ncurses.h>
 #define SCR_WD 80
 #define SCR_HG 25
 #define PDL_SIZE 3
@@ -8,57 +9,56 @@
 void Draw(int ball_x, int ball_y, int player1_pos, int player2_pos, int score1, int score2) {
     const int score_y_pos = 7;
 
-    printf("\033[H\033[2J\033[3J");  // Clear space
+    clear();  // Clear space
 
     for (int y = 0; y < SCR_HG; y++) {
         for (int x = 0; x < SCR_WD; x++) {
             if (x == ball_x && y == ball_y) {  // Ball
-                printf("*");
+                printw("*");
             } else if (x == 0 && y >= player1_pos && y < player1_pos + PDL_SIZE) {
-                printf("|");  // Left paddle
+                printw("|");  // Left paddle
             } else if (x == SCR_WD - 1 && y >= player2_pos && y < player2_pos + PDL_SIZE) {
-                printf("|");  // Right paddle
+                printw("|");  // Right paddle
             } else if (x == SCR_WD / 2) {
-                printf("|");  // Separator
+                printw("|");  // Separator
             } else if (x == SCR_WD / 2 - 3 && y == score_y_pos) {
-                int offset = printf("%d", score1);  // Score plr1
+                int offset = printw("%d", score1);  // Score plr1
                 x += offset - 1;
             } else if (x == SCR_WD / 2 + 3 && y == score_y_pos) {
-                int offset = printf("%d", score2);  // Score plr2
+                int offset = printw("%d", score2);  // Score plr2
                 x += offset - 1;
             } else
-                printf(" ");
+                printw(" ");
         }
-        printf("\n");
+        printw("\n");
     }
+    refresh();
 }
 
 void DrawWin(int winner) {
-    printf("\e[H");
+    printw("\e[H");
     if (winner == 1) {
-        printf(
+        printw(
             "  _____ ___   __          _______ _   _ _ \n |  __ \\__ \\  \\ \\        / /_   _| \\ | | |\n | "
             "|__) | ) |  \\ \\  /\\  / /  | | |  \\| | |\n |  ___/ / /    \\ \\/  \\/ /   | | | . ` | |\n | "
             "|    / /_     \\  /\\  /   _| |_| |\\  |_|\n |_|   |____|     \\/  \\/   |_____|_| \\_(_)\n     "
             "                                     \n                                          ");
     } else {
-        printf(
+        printw(
             "  _____  __  __          _______ _   _ _ \n |  __ \\/_ | \\ \\        / /_   _| \\ | | |\n | "
             "|__) || |  \\ \\  /\\  / /  | | |  \\| | |\n |  ___/ | |   \\ \\/  \\/ /   | | | . ` | |\n | |  "
             "   | |    \\  /\\  /   _| |_| |\\  |_|\n |_|     |_|     \\/  \\/   |_____|_| \\_(_)\n          "
             "                               \n                                         ");
     }
+    refresh();
 }
 
 // --- ЗОНА 2: Проверка ввода ---
 
 int Input() {
-    int input_code = -1;
+    int input_code = 0;
 
-    char kbs;
-
-    while (input_code == -1) {
-        kbs = getchar();
+    char kbs = getch();
 
         if (kbs == 'A' || kbs == 'a') {
             input_code = -11;
@@ -68,14 +68,7 @@ int Input() {
             input_code = -12;
         } else if (kbs == 'M' || kbs == 'm') {
             input_code = 12;
-        } else if (kbs == ' ') {
-            input_code = 0;
-        } else if (kbs == '\n') {
-            input_code = -2;
         }
-    }
-
-    while ((kbs = getchar()) != '\n' && kbs != EOF);
     return input_code;
 }
 
@@ -143,42 +136,51 @@ int CheckGoal(int ball_x, int ball_y) {
     return score2 * 10 + score1;
 }
 
+void interective() {
+    initscr();
+    noecho();
+    curs_set(0);
+    nodelay(stdscr,TRUE);
+    keypad(stdscr, TRUE);
+}
 int main() {
+    interective();
     int ball_x = SCR_WD / 2 - 2;
     int ball_y = SCR_HG / 2;
     int ball_x_direction = -1, ball_y_direction = 1;
     int player1_pos = SCR_HG / 2, player2_pos = SCR_HG / 2;
     int score1 = 0, score2 = 0, winner;
+    timeout(40);
+
 
     while ((winner = CheckWin(score1, score2)) == -1) {
         Draw(ball_x, ball_y, player1_pos, player2_pos, score1, score2);
 
         int input = Input();
-        if (input != -2) {
-            int which_player = DetectWhichPlayer(input);
-            int player_direction = GetDirectionByInput(input);
-            if (which_player == 1) {
-                player1_pos += HandlePaddleDirection(player_direction, player1_pos);
-            } else if (which_player == 2) {
-                player2_pos += HandlePaddleDirection(player_direction, player2_pos);
-            }
+        int which_player = DetectWhichPlayer(input);
+        int player_direction = GetDirectionByInput(input);
+        if (which_player == 1) {
+            player1_pos += HandlePaddleDirection(player_direction, player1_pos);
+        } else if (which_player == 2) {
+            player2_pos += HandlePaddleDirection(player_direction, player2_pos);
+        }
 
-            ball_x_direction = GetBallDirectionX(ball_x_direction, ball_x, ball_y, player1_pos, player2_pos);
-            ball_y_direction = GetBallDirectionY(ball_y_direction, ball_y);
-            ball_x += ball_x_direction;
-            ball_y += ball_y_direction;
+        ball_x_direction = GetBallDirectionX(ball_x_direction, ball_x, ball_y, player1_pos, player2_pos);
+        ball_y_direction = GetBallDirectionY(ball_y_direction, ball_y);
+        ball_x += ball_x_direction;
+        ball_y += ball_y_direction;
 
-            int goal_result = CheckGoal(ball_x, ball_y);
-            if (goal_result != 0) {
-                int tmp1 = goal_result % 10;
-                int tmp2 = goal_result / 10;
-                score1 += tmp1;
-                score2 += tmp2;
-                ball_x = SCR_WD / 2 + 2 * tmp1 - 2 * tmp2;
-                ball_y = SCR_HG / 2;
-            }
+        int goal_result = CheckGoal(ball_x, ball_y);
+        if (goal_result != 0) {
+            int tmp1 = goal_result % 10;
+            int tmp2 = goal_result / 10;
+            score1 += tmp1;
+            score2 += tmp2;
+            ball_x = SCR_WD / 2 + 2 * tmp1 - 2 * tmp2;
+            ball_y = SCR_HG / 2;
         }
     }
     DrawWin(winner);
+    endwin();
     return 0;
 }
